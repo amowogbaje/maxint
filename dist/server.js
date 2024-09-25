35abd6f08cc8e0c0da0383e979a6fc15291a30e3
Amowogbaje Gideon <amowogbajegideon@gmail.com>
2024-09-25 12:12:09 +0100

    vercel correctly updated

--- dist/server.js
+++ dist/server.js
@@ -1,13 +1,4 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -17,22 +8,20 @@ const dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config();
 const serverless_http_1 = __importDefault(require("serverless-http"));
 const app_1 = __importDefault(require("./app"));
-const database_1 = __importDefault(require("./config/database"));
 const PORT = process.env.PORT || 3000;
-const startServer = () => __awaiter(void 0, void 0, void 0, function* () {
-    try {
-        yield database_1.default.authenticate();
-        console.log('Connection to the database has been established successfully.');
-        app_1.default.listen(PORT, () => {
-            console.log(`Server is running on port ${PORT}`);
-        });
-    }
-    catch (error) {
-        console.error('Unable to connect to the database:', error);
-    }
-});
-// Only start the server if not in a serverless environment
-if (process.env.NODE_ENV !== 'production') {
-    startServer();
-}
+// const startServer = async () => {
+//   try {
+//     await sequelize.authenticate();
+//     console.log('Connection to the database has been established successfully.');
+//     app.listen(PORT, () => {
+//       console.log(`Server is running on port ${PORT}`);
+//     });
+//   } catch (error) {
+//     console.error('Unable to connect to the database:', error);
+//   }
+// };
+// // Only start the server if not in a serverless environment
+// if (process.env.NODE_ENV !== 'production') {
+//   startServer();
+// }
 exports.handler = (0, serverless_http_1.default)(app_1.default);
